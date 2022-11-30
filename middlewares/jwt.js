64bf7f25e57402300746d3836fada170161d3ff5
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-11-30 15:31:12 -0300

    jwt middlewares done

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,17 +1,31 @@
 const jwt = require("jsonwebtoken");
 
+const encode = (payload) => {
+    return jwt.sign(payload, 'secretcode', {expiresIn: "24h"})
+}
+
 const decode = (req, res, next) => {
     const { token } = req.body
 
-    jwt.decode(token, 'secretcode', (err, user) => {
+    req.body.decodeToken = jwt.decode(token, {  complete: true})
+
+    next()
+}
+
+const verify = (req, res, next) => {
+    const { token } = req.body
+
+    jwt.verify(token, 'secretcode', (err, payload) => {
         //if there is not token or does not match
         if(err) return res.status(401).json({error: 'error occurred while trying to verify token'})
 
-        req.body.user = user
+        req.body.payload = payload
         next()
     })
 }
 
 module.exports = {
+    verify,
+    encode,
     decode
 }
