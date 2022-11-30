148de90786d38dd7a77ff8686e05a2cf96bc9f5e
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-11-29 23:58:40 -0300

    fix passport signin

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }))
+app.use(passport.initialize())
 app.use(passport.session())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
--- passport/local-auth.js
+++ passport/local-auth.js
@@ -2,26 +2,26 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const userService = require('../services/user.js');
 const bcrypt = require('bcrypt');
-const { User } = require('../database/models/user');
+const { User } = require('../database/models');
+
+
+passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, email, password, done) => {
+    const user = new User()
+    user.email = email
+    user.password = user.encryptPassword(password)
+    await user.save()
+    done(null, user)
+}))
 
 passport.serializeUser((user, done) => {
     done(null, user.id)
 })
 
-passport.deserializeUser( async(id, done) => {
+passport.deserializeUser(async(id, done) => {
     const user = await userService.getUserById(id)
     done(null, user)
 })
-
-passport.use('local', new LocalStrategy({
-    usernameField: email,
-    passwordField: password,
-    passReqToCallback: true
-}, async (req, email, password, done) => {
-    const user = {
-        email,
-        password
-    }
-    await User.create(user)
-    done(null, user)
-}))
