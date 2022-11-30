a23dad923b7c65cc360dd504ff94ce8adad6f46a
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-11-30 00:11:51 -0300

    add auth route in index

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ const router = express.Router()
 // example of a route with index controller get function
 router.get('/', get)
 
+const auth = require('./authentication')
+router.use('/auth', auth)
+
 module.exports = router
