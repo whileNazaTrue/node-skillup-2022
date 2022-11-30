581eba7b95a7bd5cad44fb90ce3821f2e25ce85a
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-11-30 16:09:54 -0300

    add isUser & isAdmin + format

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -5,25 +5,49 @@ const encode = (payload) => {
 };
 
 const decode = (req, res, next) => {
-    const token = req.headers["authorization"]
+    const token = req.headers["authorization"];
 
     req.body.decodedToken = jwt.decode(token, { complete: true });
 
     next();
 };
 
-const verify = (req, res, next) => {
-    const token = req.headers["authorization"]
+const isUser = (req, res, next) => {
+    const token = req.headers["authorization"];
 
-    jwt.verify(token, "secretcode", (err, payload) => {
+    const user = verify(token);
+
+    if (user instanceof Error)
+        return res
+            .status(401)
+            .json({ error: "error occurred while trying to verify token" });
+
+    if (user.rolesId >= 0) return next();
+
+    return res.status(403).json({ error: "not authorized" });
+};
+
+const isAdmin = (req, res, next) => {
+    const token = req.headers["authorization"];
+
+    const user = verify(token);
+
+    if (user instanceof Error)
+        return res
+            .status(401)
+            .json({ error: "error occurred while trying to verify token" });
+
+    if (user.rolesId === 1) return next();
+
+    return res.status(403).json({ error: "not authorized" });
+};
+
+const verify = (token) => {
+    return jwt.verify(token, "secretcode", (err, payload) => {
         //if there is not token or does not match
-        if (err)
-            return res
-                .status(401)
-                .json({ error: "error occurred while trying to verify token" });
+        if (err) return new Error();
 
-        req.body.payload = payload;
-        next();
+        return payload;
     });
 };
 
