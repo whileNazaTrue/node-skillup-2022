7008d56aa23bcb019b617d2803c2bccfb1689e59
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-11-30 00:00:36 -0300

    add signin & logout

--- passport/local-auth.js
+++ passport/local-auth.js
@@ -4,6 +4,14 @@ const userService = require('../services/user.js');
 const bcrypt = require('bcrypt');
 const { User } = require('../database/models');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser(async(id, done) => {
+    const user = await userService.getUserById(id)
+    done(null, user)
+})
 
 passport.use('local-signup', new LocalStrategy({
     usernameField: 'email',
@@ -17,11 +25,16 @@ passport.use('local-signup', new LocalStrategy({
     done(null, user)
 }))
 
-passport.serializeUser((user, done) => {
-    done(null, user.id)
-})
+passport.use('local-signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, email, password, done) => {
 
-passport.deserializeUser(async(id, done) => {
-    const user = await userService.getUserById(id)
+    const user = await userService.getUserByEmail(email)
+    console.log(user);
+    if(!user) return done(null, false, console.log('no se encontro'))
+    if(!user.comparePassword(password)) return done(null, false, console.log('contrasena incorrecta'))
+
     done(null, user)
-})
+}))
