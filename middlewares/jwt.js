cdd0f0f55cc1b2fdd7eec82420e9a456ad6917f2
Ignacio Berasategui <92552909+Ignacio20022@users.noreply.github.com>
2022-12-01 18:56:46 -0300

    Feat/jsonwebtoken (#12)
    * add jwt decode
    
    * jwt middlewares done
    
    * format code
    
    * change token from body to header
    
    * add isUser & isAdmin + format
    
    * change rol id
    
    Co-authored-by: Ignacio20022 <nacho-bera-02@hotmail.com>

---
+++ middlewares/jwt.js
@@ -0,0 +1,60 @@
+const jwt = require("jsonwebtoken");
+
+const encode = (payload) => {
+    return jwt.sign(payload, "secretcode", { expiresIn: "24h" });
+};
+
+const decode = (req, res, next) => {
+    const token = req.headers["authorization"];
+
+    req.body.decodedToken = jwt.decode(token, { complete: true });
+
+    next();
+};
+
+const isUser = (req, res, next) => {
+    const token = req.headers["authorization"];
+
+    const user = verify(token);
+
+    if (user instanceof Error)
+        return res
+            .status(401)
+            .json({ error: "error occurred while trying to verify token" });
+
+    if (user.rolesId <= 2) return next();
+
+    return res.status(403).json({ error: "not authorized" });
+};
+
+const isAdmin = (req, res, next) => {
+    const token = req.headers["authorization"];
+
+    const user = verify(token);
+
+    if (user instanceof Error)
+        return res
+            .status(401)
+            .json({ error: "error occurred while trying to verify token" });
+
+    if (user.rolesId === 1) return next();
+
+    return res.status(403).json({ error: "not authorized" });
+};
+
+const verify = (token) => {
+    return jwt.verify(token, "secretcode", (err, payload) => {
+        //if there is not token or does not match
+        if (err) return new Error();
+
+        return payload;
+    });
+};
+
+module.exports = {
+    verify,
+    encode,
+    decode,
+    isUser,
+    isAdmin
+};
