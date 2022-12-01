3abdb9506bfd3789a4d0707cc491a9f63da5330d
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-01 15:37:08 -0300

    add validate result helper

---
+++ helpers/validate.js
@@ -0,0 +1,14 @@
+const { validationResult } = require("express-validator");
+
+const validateResult = (req, res, next) => {
+    try {
+        validationResult(req).throw();
+        return next();
+    } catch (error) {
+        res.status(403).json({ errors: error.array() });
+    }
+};
+
+module.exports = {
+    validateResult,
+};
