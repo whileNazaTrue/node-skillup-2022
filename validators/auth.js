f22295a8f74bfe6fcc84f7cdd1f474969955f6cd
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-01 23:31:56 -0300

    login & register validation

--- validators/auth.js
+++ validators/auth.js
@@ -1,27 +1,136 @@
-const { check } = require("express-validator");
+const { check, checkSchema } = require("express-validator");
 const { validateResult } = require("../helpers/validate");
 
 const signinValidation = [
-    check("email").exists().isEmail().normalizeEmail().not().isEmpty(),
-    check("password").exists().not().isEmpty(),
+    checkSchema({
+        email: {
+            in: ["body"],
+            exists: {
+                errorMessage: "the email is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the email cannot be empty",
+                bail: true,
+            },
+            isString: {
+                errorMessage: "the email must a string",
+                bail: true,
+            },
+            isEmail: {
+                errorMessage: "the email is invalid",
+                bail: true,
+            },
+            normalizeEmail: true,
+        },
+        password: {
+            in: ["body"],
+            exists: {
+                errorMessage: "the password is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the password cannot be empty",
+                bail: true,
+            },
+            isString: {
+                errorMessage: "the password must a string",
+                bail: true,
+            },
+        },
+    }),
     (req, res, next) => {
         validateResult(req, res, next);
     },
 ];
 
 const signupValidation = [
-    check('firstName').exists().not().isEmpty(),
-    check('lastName').exists().not().isEmpty(),
-    check("email").exists().isEmail().normalizeEmail().not().isEmpty(),
-    check("password").exists().isLength({min: 5}).not().isEmpty(),
-    check('avatar').trim().escape(),
-    // check("roleId").exists().not().isEmpty(),
+    checkSchema({
+        firstName: {
+            in: ["body"],
+            exists: {
+                errorMessage: "the first name is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the first name cannot be empty",
+                bail: true,
+            },
+            isAlpha: {
+                errorMessage: "The first name must only contain letters"
+            },
+            isString: {
+                errorMessage: "the first name must a string",
+                bail: true,
+            },
+        },
+        lastName: {
+            in: ["body"],
+            exists: {
+                errorMessage: "the last name is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the last name cannot be empty",
+                bail: true,
+            },
+            isAlpha: {
+                errorMessage: "The last name must only contain letters"
+            },
+            isString: {
+                errorMessage: "the last name must a string",
+                bail: true,
+            },
+        },
+        email: {
+            in: ["body"],
+            exists: {
+                errorMessage: "the email is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the email cannot be empty",
+                bail: true,
+            },
+            isString: {
+                errorMessage: "the email must a string",
+                bail: true,
+            },
+            isEmail: {
+                errorMessage: "the email is invalid",
+                bail: true,
+            },
+            normalizeEmail: true,
+        },
+        password: {
+            in: ["body"],
+            exists: {
+                errorMessage: "the password is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the password cannot be empty",
+                bail: true,
+            },
+            isString: {
+                errorMessage: "the password must a string",
+                bail: true,
+            },
+        },
+        avatar: {
+            in: ["body"],
+            optional: true,
+            isURL: {
+                errorMessage: "the avatar must be a link",
+            },
+        },
+    }),
     (req, res, next) => {
-        validateResult(req, res, next)
-    }
-]
+        validateResult(req, res, next);
+    },
+];
 
 module.exports = {
     signinValidation,
-    signupValidation
+    signupValidation,
 };
