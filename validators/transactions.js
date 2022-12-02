67c12010a75fac6ba1d990793fa29efa45f2bb7d
Ignacio Berasategui <92552909+Ignacio20022@users.noreply.github.com>
2022-12-02 15:46:13 -0300

    Feat/express validations (#16)
    * add validate result helper
    
    * login & resigter validations
    
    * small fix
    
    * Merge branch 'main' of https://github.com/whileNazaTrue/node-skillup-2022 into feat/express-validations
    
    * images validation
    
    * transaction validation
    
    * login & register validation
    
    * modify validation error code
    
    * add is int to amount validator
    
    * match error code
    
    Co-authored-by: Ignacio20022 <nacho-bera-02@hotmail.com>

---
+++ validators/transactions.js
@@ -0,0 +1,77 @@
+const { checkSchema } = require("express-validator");
+const { validateResult } = require("../helpers/validate");
+
+//to edit transactions endpoint
+const transactionValidation = [
+    checkSchema({
+        userId: {
+            in: ["body"],
+            exists: {
+                errorMessage: "user id is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the user id field cannot be empty",
+                bail: true,
+            },
+            isInt: {
+                errorMessage: "the id must be a number",
+                bail: true,
+            },
+        },
+        categoryId: {
+            in: ["body"],
+            exists: {
+                errorMessage: "category id is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the category id field cannot be empty",
+                bail: true,
+            },
+            isInt: {
+                errorMessage: "the id must be a number",
+                bail: true,
+            },
+        },
+        amount: {
+            in: ["body"],
+            exists: {
+                errorMessage: "the amount is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the amount field cannot be emmpty",
+                bail: true,
+            },
+            isInt: {
+                errorMessage: "the amount must be a number",
+                bail: true,
+            },
+        },
+        date: {
+            in: ["body"],
+            exists: {
+                errorMessage: "the date is missing",
+                bail: true,
+            },
+            notEmpty: {
+                errorMessage: "the date field cannot be empty",
+                bail: true,
+            },
+            //is date format = AAAA/MM/DD
+            isDate: {
+                errorMessage: "the date must be AAAA/MM/DD",
+                bail: true,
+            },
+        },
+    }),
+    (req, res, next) => {
+        validateResult(req, res, next);
+    },
+];
+
+
+module.exports = {
+    transactionValidation,
+}
