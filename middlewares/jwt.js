a184bef9ff787cd7c9409782d150e3c906e64191
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-06 20:07:59 -0300

    check for token and query

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -5,45 +5,47 @@ const encode = (payload) => {
 };
 
 const decode = (req, res, next) => {
-    const token = req.headers["authorization"];
+    const token = req.headers["authorization"]
+    if(!token) return res.status(401).json({ error: 'no token was send' })
+    const modToken = token.split(' ')[1];
 
-    req.body.decodedToken = jwt.decode(token, { complete: true });
+    req.body.decodedToken = jwt.decode(modToken, { complete: true });
 
     next();
 };
 
 const isUser = (req, res, next) => {
     const token = req.headers["authorization"];
-
+    const id = req.query.userId
+    
     const user = verify(token);
 
-    if (user instanceof Error)
-        return res
-            .status(401)
-            .json({ error: "error occurred while trying to verify token" });
+    if(user instanceof Error)return res.status(401).json({ error: "error occurred while trying to verify token" });
+    
+    if(!id && user.roleId <= 2) return next();
+    if(id && id == user.id) return next()
+    if(user.roleId === 1) return next()
 
-    if (user.rolesId <= 2) return next();
 
     return res.status(403).json({ error: "not authorized" });
 };
 
 const isAdmin = (req, res, next) => {
     const token = req.headers["authorization"];
-
     const user = verify(token);
 
-    if (user instanceof Error)
-        return res
-            .status(401)
-            .json({ error: "error occurred while trying to verify token" });
+    if (user instanceof Error)return res.status(401).json({ error: "error occurred while trying to verify token" });
 
-    if (user.rolesId === 1) return next();
+    if (user.roleId === 1) return next();
 
     return res.status(403).json({ error: "not authorized" });
 };
 
 const verify = (token) => {
-    return jwt.verify(token, "secretcode", (err, payload) => {
+    if(!token) return new Error()
+
+    const modToken = token.split(' ')[1]
+    return jwt.verify(modToken, "secretcode", (err, payload) => {
         //if there is not token or does not match
         if (err) return new Error();
 
