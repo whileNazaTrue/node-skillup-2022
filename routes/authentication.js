f9c02b71d3eb366381e88d827ba425fc739addf5
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-06 20:03:15 -0300

    move passport register to POST/users

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,46 +2,26 @@ const { Router } = require("express");
 const passport = require("passport");
 const router = Router();
 const jwtMiddleware = require('../middlewares/jwt');
+const { signupValidation, signinValidation } = require("../validators/auth");
 
 
-router.post("/signup", (req, res, next) => {
-    passport.authenticate("local-signup", {session: false}, (err, user, info) => {
+router.post("/login", signinValidation, (req, res, next) => {
+    passport.authenticate("local-signin", (err, user, message) => {
         //internal error
         if (err) return next(err);
-        if (!user)
-            return res
-                .status(400)
-                .json({ success: false, message: "user creation failed" });
-
-        return res
-            .status(200)
-            .json({ success: true, message: "user created successfully" });
-    })(req, res, next);
-});
-
-router.post("/signin", (req, res, next) => {
-    passport.authenticate("local-signin", (err, user, info) => {
-        //internal error
-        if (err) return next(err);
-        if (!user)
+        if (message)
             return res
                 .status(403)
-                .json({ success: false, message: "authentication failed" });
+                .json({ success: false, message});
 
         req.login(user, {session: false}, (loginError) => {
             if (loginError) return next(loginError);
             const token = jwtMiddleware.encode(user.dataValues)
             return res
                 .status(200)
-                .json({ success: true, message: "authentication successful", token });
+                .json({ success: true, message: "authentication successful", user: user.dataValues, token });
         });
     })(req, res, next);
-});
-
-router.get("/logout", (req, res) => {
-    res.clearCookie("connect.sid", { path: "/" })
-        .status(200)
-        .json({ success: true, message: "user logout successfully" });
 });
 
 module.exports = router;
