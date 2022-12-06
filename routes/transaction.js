9aad3d9299ca6ffe960a3d00b1564dee1591a4f5
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-06 20:09:18 -0300

    /transactions add auth middlewares & POST validations

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 const transactionController = require('../controllers/transaction.js');
+const { isAdmin, isUser, decode } = require('../middlewares/jwt.js');
+const { transactionValidation } = require('../validators/transactions.js');
+
+router.use(passport.authenticate('jwt'))
 
 router
-    .get('/', transactionController.getTransactions)
-    .get('/:id', transactionController.getTransactionById)
-    .get('/user/:id', transactionController.getTransactionsByUserId)
-    .post('/', transactionController.createTransaction)
+    .get('/', isUser, transactionController.getTransactions)
+    .get('/:id', isAdmin, transactionController.getTransactionById)
+    .get('/user/:id', decode, isUser, transactionController.getTransactionsByUserId)
+    .post('/', transactionValidation, transactionController.createTransaction)
     .put('/:id', transactionController.updateTransaction)
     .delete('/:id', transactionController.deleteTransaction);
 
