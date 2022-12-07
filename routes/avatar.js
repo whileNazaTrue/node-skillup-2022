5e9f3e6ecac25a73c5ca893c5c7d133fff0f19f5
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-07 19:26:08 -0300

    add image validator to POST/avatars

--- routes/avatar.js
+++ routes/avatar.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const uploadAvatar = require('../helpers/avatar.js');
 const avatarController = require('../controllers/avatar.js');
+const { validateImageExtension } = require('../validators/images.js');
 
 
 router
     .get('/', avatarController.getAvatars)
-    .post('/', uploadAvatar.single('myfile'), avatarController.createAvatar)
+    .post('/', uploadAvatar.single('myfile'), validateImageExtension, avatarController.createAvatar)
     .get('/:id', avatarController.getAvatarById)
     .delete('/:id', avatarController.deleteAvatar);
 
