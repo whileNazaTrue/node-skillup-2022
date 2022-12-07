f26cde4150dd49769bd6d22726239efbefaca451
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-07 19:38:21 -0300

    add auth to /avatars

--- routes/avatar.js
+++ routes/avatar.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const uploadAvatar = require('../helpers/avatar.js');
 const avatarController = require('../controllers/avatar.js');
 const { validateImageExtension } = require('../validators/images.js');
+const passport = require('passport');
+const { isAdmin, isUser } = require('../middlewares/jwt.js');
 
+router.use(passport.authenticate('jwt'))
 
 router
-    .get('/', avatarController.getAvatars)
+    .get('/', isAdmin, avatarController.getAvatars)
     .post('/', uploadAvatar.single('myfile'), validateImageExtension, avatarController.createAvatar)
     .get('/:id', avatarController.getAvatarById)
     .delete('/:id', avatarController.deleteAvatar);
