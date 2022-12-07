022777cbdf2bcefb7a3753f134f042ecd08801a3
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-07 19:51:18 -0300

    remove unused decodeToken

--- routes/transaction.js
+++ routes/transaction.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const router = express.Router();
 
 const transactionController = require('../controllers/transaction.js');
-const { isAdmin, isUser, decode } = require('../middlewares/jwt.js');
+const { isAdmin, isUser } = require('../middlewares/jwt.js');
 const { transactionValidation } = require('../validators/transactions.js');
 
 router.use(passport.authenticate('jwt'))
@@ -11,7 +11,7 @@ router.use(passport.authenticate('jwt'))
 router
     .get('/', isUser, transactionController.getTransactions)
     .get('/:id', isAdmin, transactionController.getTransactionById)
-    .get('/user/:id', decode, isUser, transactionController.getTransactionsByUserId)
+    .get('/user/:id', isUser, transactionController.getTransactionsByUserId)
     .post('/', transactionValidation, transactionController.createTransaction)
     .put('/:id', transactionController.updateTransaction)
     .delete('/:id', transactionController.deleteTransaction);
