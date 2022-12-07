cc66fd847d2df897fee956c7305b7cc1e6e4f871
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-07 19:43:39 -0300

    add auth to /categories

--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
-
 const categoryController = require('../controllers/category')
+const { isAdmin } = require('../middlewares/jwt')
 
 router
     .get('/', categoryController.getCategories)
     .get('/:id', categoryController.getCategoryById)
+
+router.use(isAdmin)
+
+router
     .post('/', categoryController.createCategory)
     .put('/:id', categoryController.updateCategory)
     .delete('/:id', categoryController.deleteCategory)
