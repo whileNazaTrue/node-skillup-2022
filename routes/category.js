b9e1e0a97e92285764595b7f9395baa61421558c
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-12-08 17:09:43 -0300

    fix categories route middleware

--- routes/category.js
+++ routes/category.js
@@ -1,14 +1,14 @@
 const express = require('express')
+const passport = require('passport')
 const router = express.Router()
 const categoryController = require('../controllers/category')
 const { isAdmin } = require('../middlewares/jwt')
 
-router
-    .get('/', categoryController.getCategories)
-    .get('/:id', categoryController.getCategoryById)
 
 router
-    .use(isAdmin)
+    .use(passport.authenticate('jwt'), isAdmin)
+    .get('/', categoryController.getCategories)
+    .get('/:id', categoryController.getCategoryById)
     .post('/', categoryController.createCategory)
     .put('/:id', categoryController.updateCategory)
     .delete('/:id', categoryController.deleteCategory)
