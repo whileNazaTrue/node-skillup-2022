e4cf0e5aa49b44b9132907b481487ec543473ad6
Ignacio Berasategui <92552909+Ignacio20022@users.noreply.github.com>
2022-12-08 16:56:07 -0300

    Fix/transaction validation (#32)
    * fix compare password
    
    * add higher than 0 validation to amount
    
    Co-authored-by: Ignacio20022 <nacho-bera-02@hotmail.com>

--- validators/transactions.js
+++ validators/transactions.js
@@ -48,6 +48,13 @@ const transactionValidation = [
                 errorMessage: "the amount must be a number",
                 bail: true,
             },
+            custom:{
+                options: (value) => {
+                    if(value > 0) return Promise.resolve()
+                    else return Promise.reject('The amount must be higher than 0')
+                },
+                bail: true
+            }
         },
         description: {
             in: ["body"],
