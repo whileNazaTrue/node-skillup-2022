813455d12845cd1f56f68050577cd8895fd9e416
whileNazaTrue <nazarenoorodriguez@gmail.com>
2022-12-08 16:03:43 -0300

    fix middleware use

--- routes/user.js
+++ routes/user.js
@@ -5,16 +5,20 @@ const router = express.Router();
 const userController = require('../controllers/user.js');
 const { isAdmin, isUser } = require('../middlewares/jwt.js');
 const { signupValidation } = require('../validators/auth.js');
+const { get } = require('./authentication.js');
 
 router.post('/', signupValidation, userController.createUser)
     
 router.use(passport.authenticate('jwt'))
 
-router.get('/', isAdmin, userController.getUsers)
 
-router
+
+router.use(isAdmin)
     .get('/:id', userController.getUserById)
     .put('/:id', userController.updateUser)
-    .delete('/:id', userController.deleteUser);
+    .delete('/:id', userController.deleteUser)
+    .get('/', userController.getUsers)
+
+    
 
 module.exports = router;
