ae865ee87ebf4b2a3d4a513dd75805cc862c29ad
whileNazaTrue <nazarenoorodriguez@gmail.com>
2022-12-10 19:12:17 -0300

    Fixed deletedAt not being created in avatar

--- database/migrations/20221207143622-create-avatar.js
+++ database/migrations/20221207143622-create-avatar.js
@@ -21,6 +21,11 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null
       }
     });
   },
